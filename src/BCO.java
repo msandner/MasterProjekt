559a3db65b9682aafc7720d1a4f45308e775b839
DINO\Maren <mlsandner@gmail.com>
2018-11-14 17:43:40 +0100

    teil der funktionen implememtiert

--- src/BCO.java
+++ src/BCO.java
@@ -1,42 +1,79 @@
 import com.hsh.Evaluable;
 import com.hsh.Fitness;
 import com.hsh.parser.Dataset;
+import com.hsh.parser.Node;
 import com.hsh.parser.Parser;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class BCO {
+
+    private int alpha = 1;
+    private int beta = 10;
+    private double gamma = 0.99;
+
+    //favorisierte nächste Stadt F
+    Node favouredCity;
+    //Set mit möglichen nächsten Städten A
+    ArrayList<Node> allowedCities = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         String pathToData = "a280.tsp";
         Dataset dataset = Parser.read(pathToData);
         int cities = dataset.getSize();
 
-        ArrayList<Evaluable> path = initializePath(cities);
-
         Fitness fitness = new Fitness(dataset);
+
+        ArrayList<Evaluable> path = initializePath(cities);
         fitness.evaluate(path);
     }
 
-    /*initialisiert einen Pfad und gibt diesen als List zurück*/
+    /*initialisiert einen Pfad und gibt diesen als ArrayList zurück*/
     public static ArrayList<Evaluable> initializePath(int cities) throws IOException {
         ArrayList<Evaluable> initialpath = new ArrayList<>();
 
         Integer[] patharray = new Integer[cities];
 
-        for(int i = 0; i < cities; i++) {
-            patharray[i] = i+1;
+        for (int i = 0; i < cities; i++) {
+            patharray[i] = i + 1;
         }
 
         Collections.shuffle(Arrays.asList(patharray));
 
         Path initial = new Path(patharray);
         initialpath.add(initial);
-        
+
         return initialpath;
     }
+
+    /*city i = aktuelle Stadt
+    * city j = Stadt mit der verglichen werden soll
+    * noch nicht herausgefunden, wie t in die Gleichung mit einspielt*/
+    public double arcfitness(Node cityi, Node cityj, int t) {
+        int AunionF = 0;
+        //wenn accessable Cities A und favoured City F eine gemeinsame Instanz haben, dann AunionF auf 1 Setzen
+        if(allowedCities.contains(favouredCity)) {
+            AunionF = 1;
+        }
+
+        if(cityj.equals(favouredCity)) {
+            return gamma;
+        } else {
+            double a = 1 - gamma * AunionF;
+            double b = allowedCities.size() - AunionF;
+            return a/b;
+        }
+    }
+
+    //unter der Vermutung das die Summe von p_ij(t) = 1 ist
+    public double stateTransitionProbability(Node cityi, Node cityj, int t) {
+        double arcfitness = Math.pow(arcfitness(cityi, cityj, t), alpha);
+        double distance= Math.pow(1/cityi.distance(cityj), beta);
+        return (arcfitness * distance) / (Math.pow(1, alpha) * distance);
+    }
+
+    
 }
 
 class Path extends Evaluable {
