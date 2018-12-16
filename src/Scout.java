363676beb0986129f03d6fe5091ae8994a8c5aeb
DINO\Maren <mlsandner@gmail.com>
2018-12-16 16:22:24 +0100

    kleine Änderungen an Kommentaren

--- src/Scout.java
+++ src/Scout.java
@@ -9,6 +9,7 @@ public class Scout extends Bee {
     private BeeColony colony;
     private Integer[] favouredPath;
 
+    //ID so gesetzt, dass sie nach den "normalen" Bienen anfangen
     public Scout(int ID, BeeColony colony, Dataset dataset) {
         super(ID, colony, dataset);
         this.ID = ID + dataset.getSize();
@@ -16,11 +17,13 @@ public class Scout extends Bee {
         this.favouredPath = super.getPath();
     }
 
+    //kein observeDance, dafür wird der favouredPath immer wieder auf einen neuen Zufallspfad gesetzt
     @Override public void mainProcedure() {
         performWaggleDance(searchNewPath());
         favouredPath = newRandomFavouredPath();
     }
 
+    //zufälligen Pfad erstellen
     public Integer[] newRandomFavouredPath() {
         Integer[] pathArray = colony.getDefaultArray().clone();
         Collections.shuffle(Arrays.asList(pathArray));
